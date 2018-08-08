29cfeadc3e9a64445a2c83954ae9e4417e90c387
maheshkumar <maheshkumar.akoju@gmail.com>
2018-08-08 20:27:12 +0530

    modified constants page

--- src/main/java/com/crossover/dataconstants/Constants.java
+++ src/main/java/com/crossover/dataconstants/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     public static final String GMAIL_URL = "https://accounts.google.com/ServiceLogin/signinchooser?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
     public static final String BROWSER = "chrome";
     public static final String USERNAME = "xxxxxxx@gmail.com";
-    public static final String PASSWORD = "Nidhi@1206";
+    public static final String PASSWORD = "xxxxxxx";
     public static final String TOADDRESS = "xxxxxxx@gmail.com";
     public static final String SUBJECT = "Automation";
     public static final String MESSAGEBODY = "Automation Test Result";
