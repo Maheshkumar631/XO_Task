292fad4f88b23366586268504b72ec1e05a4596e
maheshkumar <maheshkumar.akoju@gmail.com>
2018-08-08 19:26:44 +0530

    modified names

---
+++ src/test/java/com/crossover/e2e/TestBase.java
@@ -0,0 +1,39 @@
+package com.crossover.e2e;
+
+import com.crossover.common.BrowserFunction;
+import com.crossover.common.ReportFunction;
+import com.crossover.dataconstants.Constants;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+
+
+/**
+ * @author Mahesh akoju
+ */
+public class TestBase {
+    WebDriver driver;
+    ExtentReports report;
+    ExtentTest test;
+    ReportFunction reportFunction ;
+    BrowserFunction browserFunction = new BrowserFunction();
+    Constants constants = new Constants();
+
+    String filePath = System.getProperty("user.dir") + "/Results/AutomationReport.html";
+
+    public TestBase(){
+        reportFunction = new ReportFunction();
+        report = reportFunction.getInstance(filePath);
+        driver = browserFunction.launchBrowser(constants.BROWSER);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+        report.flush();
+    }
+
+
+}
+
